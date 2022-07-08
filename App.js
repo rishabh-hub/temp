faf4ebef46ee8e839d94ed932c3959702bd59be7
rishabh-hub <rishabh.1056@gmail.com>
2022-07-08 21:04:47 +0530

    Promises learnt

--- App.js
+++ App.js
@@ -16,10 +16,86 @@
 
 // server.listen(5000)
 
-const _ = require('lodash');
+// const _ = require('lodash');
 
-const items = [1 , [2,[3,[4]]]]
+// const items = [1 , [2,[3,[4]]]]
 
-const newItems=_.flattenDeep(items);
-console.log(newItems);
-console.log("Hello People")
+// const newItems=_.flattenDeep(items);
+// console.log(newItems);
+// console.log("Hello People")
+
+// const http=require("http");
+
+// const server=http.createServer((req,res)=>{
+//     if(req.url==='/')
+//     {
+//         res.end('Home Page');
+//     }
+
+//     else if(req.url==='/about')
+//     {
+//         //BLOCKING CODE
+//         for(let i=0;i<1000;i++)
+//         {
+//             for(let j=0;j<1000;j++)
+//             {
+//                 console.log(j+" "+i);
+//             }
+//         }
+
+//         res.end('About Page');
+//     }
+
+//     else{
+//         res.end('Error')
+//     }
+// });
+
+// server.listen(5000,()=>{
+//     console.log("Server is listening on port:5000");
+// })
+
+const {readFile,writeFile} = require("fs").promises;
+
+// const getText = (path) => // a promise
+// {
+//     return new Promise((resolve,reject)=>{
+        
+//         readFile(path,"utf-8",(err,data)=>
+//             {
+//                 if(err)
+//                 {
+//                     reject(err);
+//                 }
+//                 else{
+//                     resolve(data);
+//                 }
+//             });
+//     });
+// }
+
+// getText('./content/first.txt').then((result) => console.log(result)).catch((err)=>console.log(err));
+
+const util =require("util");
+const readFilePromise=util.promisify(readFile);
+const writeFilePromise=util.promisify(writeFile);
+
+
+const start = async()=>{
+
+    try{
+        const first = await readFile('./content/first.txt',"utf-8");
+        const second = await readFile("./content/second.txt","utf-8");
+        await writeFile('./content/third.txt','This is data for third file'+first+" "+second,{flag:'a'});
+        console.log(first);
+        console.log(second);
+
+
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+
+start();
